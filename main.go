35d2984a85888fdbdabc7d9f38294eb435fc1aed
Neverlock <neverlock@gmail.com>
2016-08-28 16:30:00 +0000

    use native websocket

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/kataras/iris"
+	"github.com/neverlock/utility/random"
+	"golang.org/x/net/websocket"
 )
+
+/* Native messages no need to import the iris-ws.js to the ./templates.client.html
+Use of: OnMessage and EmitMessage
+*/
 
 type clientPage struct {
 	Title string
@@ -16,6 +23,7 @@ type BookingAPI struct {
 }
 
 func main() {
+
 	iris.Static("/js", "./static/js", 1)
 
 	iris.Get("/", func(ctx *iris.Context) {
@@ -25,26 +33,22 @@ func main() {
 
 	// the path which the websocket client should listen/registed to ->
 	iris.Config.Websocket.Endpoint = "/my_endpoint"
-	// for Allow origin you can make use of the middleware
-	//iris.Config().Websocket.Headers["Access-Control-Allow-Origin"] = "*"
 
-	var myChatRoom = "room1"
-	iris.Websocket.OnConnection(func(c iris.WebsocketConnection) {
+	ws := iris.Websocket // get the websocket server
 
-		c.Join(myChatRoom)
+	ws.OnConnection(func(c iris.WebsocketConnection) {
 
-		c.On("chat", func(message string) {
-			// to all except this connection ->
-			//c.To(websocket.Broadcast).Emit("chat", "Message from: "+c.ID()+"-> "+message)
+		c.OnMessage(func(data []byte) {
+			message := string(data)
+			//c.To(iris.Broadcast).EmitMessage([]byte("Message from: " + c.ID() + "-> " + message))
+			//c.EmitMessage([]byte("Me: " + message))
+			height := random.Int(1, 100)
+			width := random.Int(1, 300)
 
-			// to the client ->
-			//c.Emit("chat", "Message from myself: "+message)
-
-			//send the message to the whole room,
-			//all connections are inside this room will receive this message
-			//c.To(myChatRoom).Emit("chat", "From: "+c.ID()+": "+message)
-			js := fmt.Sprintf("{\"From\":\"%s\",\"MSG\":\"%s\"}", c.ID(), message)
-			c.To(myChatRoom).Emit("chat", js)
+			js := fmt.Sprintf("{\"From\":\"%s\",\"H\":%d,\"W\":%d,\"MSG\":\"%s\"}", c.ID(), height, width, message)
+			c.To(iris.Broadcast).EmitMessage([]byte(js))
+			c.EmitMessage([]byte(js))
+			//c.To(myChatRoom).Emit("chat", js)
 		})
 
 		c.OnDisconnect(func() {
@@ -57,4 +61,29 @@ func main() {
 
 func (u BookingAPI) Get() {
 	u.Write("Get from /booking")
+	origin := "http://104.238.149.36:8080/"
+	url := "ws://104.238.149.36:8080/my_endpoint"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*
+		Fm := "Test from"
+		Msg := "dummy msg"
+		js1 := fmt.Sprintf("{\"From\":\"%s\",\"MSG\":\"%s\"}", Fm, Msg)
+	*/
+	//height := random.Int(1, 100)
+	//width := random.Int(1, 300)
+
+	//js := fmt.Sprintf("{\"From\":\"%s\",\"H\":%d,\"W\":%d,\"MSG\":\"%s\"}", c.ID(), height, width, message)
+
+	if _, err := ws.Write([]byte("")); err != nil {
+		log.Fatal(err)
+	}
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
 }
--- static/js/chat.js
+++ static/js/chat.js
@@ -7,27 +7,30 @@ $(function () {
 	messages = $("#messages");
 
 
-	w = new Ws("ws://" + HOST + "/my_endpoint");
-	w.OnConnect(function () {
-		console.log("Websocket connection established");
-	});
+	w = new WebSocket("ws://" + HOST + "/my_endpoint");
+	w.onopen = function () {
+		console.log("Websocket connection enstablished");
+	};
 
-	w.OnDisconnect(function () {
+	w.onclose = function () {
 		appendMessage($("<div><center><h3>Disconnected</h3></center></div>"));
-	});
+	};
+	w.onmessage = function(message){
+		//appendMessage($("<div>" + message.data + "</div>"));
+		//
+                //console.log(message)
+                //console.log(message.data)
+		var json = JSON.parse(message.data);
+                console.log(json.From + ": " + json.MSG)
+                var x = Math.floor(Math.random() * 300);
+                var y = Math.floor(Math.random() * 100);
+                test1(x,y);
+	};
 
-	w.On("chat", function (message) {
-		//appendMessage($("<div>" + message + "</div>"));
-		//hack render img
-		var json = JSON.parse(message);
-		console.log(json.From + ": " + json.MSG)
-		var x = Math.floor(Math.random() * 300);
-		var y = Math.floor(Math.random() * 100);
-		test1(x,y);
-	});
 
 	$("#sendBtn").click(function () {
-		w.Emit("chat", messageTxt.val().toString());
+		//w.EmitMessage(messageTxt.val());
+		w.send(messageTxt.val().toString());
 		messageTxt.val("");
 	});
 
@@ -52,7 +55,7 @@ function test1() {
         var x = arguments[0]
         var y = arguments[1]
 
-	/*
+        /*
         var t0 = new Date().getTime();
                 var r = 255;
                 var g = 0;
@@ -62,15 +65,15 @@ function test1() {
                 ctx.fillRect(x, y, 1, 1);
         var t1 = new Date().getTime();
         console.log('fillRect() method: ' + (t1 - t0));
-	*/
+        */
 
 
         id.data[3] = 255;
         var t0 = new Date().getTime();
-		var r = 255;
+                var r = 255;
                 var g = 0;
                 var b = 0;
- 
+
                 id.data[0] = r;
                 id.data[1] = g;
                 id.data[2] = b;
